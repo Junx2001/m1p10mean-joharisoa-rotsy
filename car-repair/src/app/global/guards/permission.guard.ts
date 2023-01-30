6c0eb67257bc963758a7788f277816c8101bc32c
unknown <rafa.rotsy@gmail.com>
2023-01-30 22:06:41 +0300

    manage permission observable

--- car-repair/src/app/global/guards/permission.guard.ts
+++ car-repair/src/app/global/guards/permission.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
 import { Observable } from "rxjs";
 import { UserService } from "../services/user.service";
-
+import { map } from "rxjs/operators";
 @Injectable({
     providedIn: 'root'
   })
@@ -13,27 +13,27 @@ export class PermissionGuard implements CanActivate{
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
         let routeChildren = state.url.split("/")[1];
         
-        this.userService.getCurrentUser().subscribe(
-            response => {
-              if (response.user.role==='ROLE_USER_FINANCE'){
-                if (routeChildren === 'atelier'){
-                  this.router.navigateByUrl('/forbidden');
-                  return false;
-                }
-              }else if (response.user.role==='ROLE_USER_ATELIER'){
-                if (routeChildren === 'finance'){
-                  this.router.navigateByUrl('/forbidden');
-                  return false;
-                }
-              }else{
-                if (routeChildren === 'finance' || routeChildren === 'atelier'){
-                  this.router.navigateByUrl('/forbidden');
-                  return false;
-                }
-              }  
-            }
-          );
-          return true;
-        
+        const bool = this.userService.getCurrentUser().pipe(
+          map((response) => {
+            if (response.user.role==='ROLE_USER_FINANCE'){
+              if (routeChildren === 'atelier'){
+                this.router.navigateByUrl('/forbidden');
+                return false;
+              }
+            }else if (response.user.role==='ROLE_USER_ATELIER'){
+              if (routeChildren === 'finance'){
+                this.router.navigateByUrl('/forbidden');
+                return false;
+              }
+            }else{
+              if (routeChildren === 'finance' || routeChildren === 'atelier'){
+                this.router.navigateByUrl('/forbidden');
+                return false;
+              }
+            }  
+            return true;
+          })
+        );
+        return bool;
     }
 }
